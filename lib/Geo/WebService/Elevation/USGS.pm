4deb79fa7d8beef1ca72c5f9db35eb19156aa17d
Tom Wyant <wyant@cpan.org>
2012-01-07 04:18:41 +0000

    Import of WYANT/Geo-WebService-Elevation-USGS-0.010 from CPAN.
    gitpan-cpan-distribution: Geo-WebService-Elevation-USGS
    gitpan-cpan-version:      0.010
    gitpan-cpan-path:         WYANT/Geo-WebService-Elevation-USGS-0.010.tar.gz
    gitpan-cpan-author:       WYANT
    gitpan-cpan-maturity:     released

--- lib/Geo/WebService/Elevation/USGS.pm
+++ lib/Geo/WebService/Elevation/USGS.pm
@@ -108,7 +108,7 @@ use LWP::UserAgent;
 use Scalar::Util 1.10 qw{ blessed looks_like_number };
 use XML::Parser;
 
-our $VERSION = '0.009';
+our $VERSION = '0.010';
 
 use constant BEST_DATA_SET => -1;
 
@@ -1346,7 +1346,7 @@ Thomas R. Wyant, III; F<wyant at cpan dot org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2008-2010, Thomas R. Wyant, III
+Copyright (C) 2008-2012 Thomas R. Wyant, III
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl 5.10.0. For more details, see the full text
